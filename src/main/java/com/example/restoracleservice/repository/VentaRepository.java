a4dcc5ab4d4a319461dda6d92efcfa973b0ddd67
gtapiero <giovanny.tapiero@pi.edu.co>
2025-12-18 18:04:02 -0500

    commit - VentaRepository .

--- src/main/java/com/example/restoracleservice/repository/VentaRepository.java
+++ src/main/java/com/example/restoracleservice/repository/VentaRepository.java
@@ -1,4 +1,26 @@
 package com.example.restoracleservice.repository;
 
+import com.example.restoracleservice.model.VentaRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class VentaRepository {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public void registrarVenta(VentaRequest request) {
+        jdbcTemplate.update(
+                "CALL SP_REGISTRAR_VENTA(?,?,?,?,?,?,?)",
+                request.getCliente().getIdCliente(),
+                request.getCliente().getNombre(),
+                request.getCliente().getEmail(),
+                request.getProducto().getIdProducto(),
+                request.getProducto().getNombre(),
+                request.getProducto().getPrecio(),
+                request.getDetalleProducto()
+        );
+    }
 }
