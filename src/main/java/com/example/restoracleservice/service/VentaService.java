7f97733f87f85eac580c68b3719d51957e9c8f9a
gtapiero <giovanny.tapiero@pi.edu.co>
2025-12-18 18:05:54 -0500

    commit - VentaService .

--- src/main/java/com/example/restoracleservice/service/VentaService.java
+++ src/main/java/com/example/restoracleservice/service/VentaService.java
@@ -1,4 +1,17 @@
 package com.example.restoracleservice.service;
 
+import com.example.restoracleservice.model.VentaRequest;
+import com.example.restoracleservice.repository.VentaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class VentaService {
+
+    @Autowired
+    private VentaRepository ventaRepository;
+
+    public void procesarVenta(VentaRequest request) {
+        ventaRepository.registrarVenta(request);
+    }
 }
