48f4a9953a21b0239cacd2ec39c1ff758271bb83
gtapiero <giovanny.tapiero@pi.edu.co>
2025-12-18 18:00:42 -0500

    commit - Cliente .

--- src/main/java/com/example/restoracleservice/model/Producto.java
+++ src/main/java/com/example/restoracleservice/model/Producto.java
@@ -1,4 +1,32 @@
 package com.example.restoracleservice.model;
 
 public class Producto {
+
+    private int idProducto;
+    private String nombre;
+    private double precio;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
 }
--- src/main/java/com/example/restoracleservice/model/VentaRequest.java
+++ src/main/java/com/example/restoracleservice/model/VentaRequest.java
@@ -1,4 +1,32 @@
 package com.example.restoracleservice.model;
 
 public class VentaRequest {
+
+    private Cliente cliente;
+    private Producto producto;
+    private String detalleProducto;
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public String getDetalleProducto() {
+        return detalleProducto;
+    }
+
+    public void setDetalleProducto(String detalleProducto) {
+        this.detalleProducto = detalleProducto;
+    }
 }
