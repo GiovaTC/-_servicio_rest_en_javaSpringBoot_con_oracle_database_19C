ba46865264d8d19a9f733842bbb61adb8a944ea4
gtapiero <giovanny.tapiero@pi.edu.co>
2025-12-18 18:10:46 -0500

    commit - VentaController .

--- src/main/java/com/example/restoracleservice/controller/VentaController.java
+++ src/main/java/com/example/restoracleservice/controller/VentaController.java
@@ -1,4 +1,24 @@
 package com.example.restoracleservice.controller;
 
+import com.example.restoracleservice.model.VentaRequest;
+import com.example.restoracleservice.service.VentaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/ventas")
 public class VentaController {
+
+    @Autowired
+    private VentaService ventaService;
+
+    @PostMapping("/registrar")
+    public ResponseEntity<?> registrarVenta(@RequestBody VentaRequest request){
+        ventaService.procesarVenta(request);
+        return ResponseEntity.ok("venta registrada exitosamente en oracle 19C .");
+    }
 }
